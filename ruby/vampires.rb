0425d4b5b3dd3a90678327440c825c375410c0cd
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-12 19:07:15 -0500

    Write new employee questionaire

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+name = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?(yes/no)"
+garlic_bread = gets.chomp.downcase
+puts "Would you like to enroll in the company’s health insurance?(yes/no)"
+health_insurance = gets.chomp.downcase
