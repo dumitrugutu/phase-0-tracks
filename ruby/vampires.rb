0a09965f1d8ee588936b45435703c87a7aa1274f
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-15 01:28:43 -0500

    Turn asnwers about age into booleans

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,16 +1,37 @@
+# Employee questionaire
 puts "How many employees do you want to process?"
 employee_form = gets.chomp.to_i
 
+# Repeat the process until each employee has completed the form
 until employee_form == 0
 
+  # Collect personal information
   puts "What is your name?"
   name = gets.chomp
+  
+  # Convert string into an integer
+  puts "What year were you born?"
+  year_born = gets.chomp.to_i
+
+  # Convert string into an integer
   puts "How old are you?"
   age = gets.chomp.to_i
-  puts "Our company cafeteria serves garlic bread. Should we order some for you?(yes/no)"
+
+  # Verify age
+  current_year = 2016
+  if (current_year - year_born) == age
+    age = true
+  else
+    age = false
+  end
+
+  puts "Do you like eating garlic_bread for lunch?(yes/no)"
   garlic_bread = gets.chomp.upcase
+
   puts "Would you like to enroll in the company’s health insurance?(yes/no)"
   health_insurance = gets.chomp.upcase
+
+  # Subtract 1 each time going through the loop
   employee_form = employee_form - 1
 
 secret_allergy = "sunshine"
