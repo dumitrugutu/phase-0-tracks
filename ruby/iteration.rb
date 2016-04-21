1ce5902b48e45a3259da8ba7112997b8f2326811
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-20 22:23:03 -0500

    Add comments to explain the code

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,19 +7,23 @@ my_hash = {
 		"car" => "transportation"
 }
 
+# iterate through the array using .each
 my_array.each do |item|
 	puts item
 end
 
+# iterate through the hash using .each
 my_hash.each do |word, use|
 	puts "#{word} is #{use}"
 end
 
+# iterate through the array and change last item using .map
 my_array.map! do |item|
 	puts item
 	my_array.replace(["redbull", "pbj", "books"]) 
 end
 
+# iterates through deleting any words that have less than 4 letters
 delete_if_array = ["boom", "children", "beer", "apples", "two", "foo"]
 
 random_array.each do |word|
@@ -28,6 +32,7 @@ end
 
 p random_array
 
+# iterates through keeping any words that have less than 4 letters
 keep_if_array = ["boom", "children", "beer", "apples", "two", "foo"]
 
 random_array.each do |word|
@@ -36,9 +41,8 @@ end
 
 p keep_if_array
 
-
+# iterates through rejecting any words that have more than 5 letters
 reject_if_array = ["boom", "children", "beer", "apples", "two", "foo"]
-container_array = []
 
 reject_if_array.each do |word|
     reject_if_array.reject! { |word| word.length > 5}
