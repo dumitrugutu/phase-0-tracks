cb713392e1ced1f0710c2256fa9d8be27b3b2552
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-21 16:46:13 -0500

    Initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,9 @@
+# take full name "Felicia Torres"
+
+# swap first name with last name "Torres Felicia"
+
+# replace every vowel("aeiou") in the name
+# with the next vowel in ("aeiou")
+
+# replace every consonant("bcdfghjklmnpqrstvxzwy") 
+# in the name with the next consonant
