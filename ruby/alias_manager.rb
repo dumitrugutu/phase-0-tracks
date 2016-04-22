f14b845db764141d460b5d7b9208372861b5be1b
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-22 12:34:55 -0500

    Remove commented out code

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,34 +2,30 @@
 puts "Please enter your full name:"
 full_name = gets.chomp
 
-p full_name
-# puts "Please enter your last name:"
-# last_name = gets.chomp.downcase
-
 # split first name into individual letters
 split_full_name = full_name.split('')
 p split_full_name
-# # split last name into individual letters
-# split_last_name = last_name.split('')
 
 # create vowel and consonant variables
 vowels = "aeiouAEIOU"
 consonants = "bcdfghjklmnpqrstvxzwyBCDFGHJKLMNPQRSTVXZWY"
+# create a container to collect the replaced letters
 full_alias = ""
 
-# compare each letter in the first name
-# to the list of vowels
+# compare each letter in the name with the string of vowels
 split_full_name.map! do |letter|
 	# replace the letter "u" with "a"
 	if letter == "u"
 		full_alias += "a"
+	# replace the letter "y" with "b"
 	elsif letter == "y"
 	    full_alias += "b"
+	# do nothing to the empty space
 	elsif letter == " "
 	    full_alias += " "
 	# if the current letter is a vowel
 	elsif vowels.include?(letter)
-		# replace the vowel with the next vowel
+		# replace the vowel with the next vowel from the string
 		full_alias += vowels[vowels.index(letter) + 1]
 	else
 		# replace the consonant with the next consonant
