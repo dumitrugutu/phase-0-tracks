764000221bbeff7ed8ed16aea2ab38951686a6ab
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-26 11:39:14 -0500

    Comment puppy's characteristics and behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# Species -------
+# Furrus Bellyrubus
+
+# Characteristics -------
+# Name: varies
+# Fur color: varies from dark brown to fair
+# Fur length: varies from .5 inches to 3.5 inches
+# Friendly: very
+# Food: dog food, milk, shoe laces
+
+# Behavior -------
+# Likes belly rubs
+# Clumsy
+# Chews on household objects(shoe laces)
+# Bothers the cat
