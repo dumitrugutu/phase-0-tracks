b7f6be0b1cdaf9aa48a0771fb7fe433b60292c64
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-26 16:55:44 -0500

    Add methods to the pupy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,33 @@ class Puppy
     toy
   end
 
+  def speak(number)
+  	number.times {|num| print "Woof!" }
+  	puts
+  	number
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(integer)
+  	integer * 7
+  end
+
+  def play_dead(time)
+  	time.times do 
+  	  puts "I'm dead"	
+  	end
+    puts "I'm alive!!!"	
+  end
+
 end
+
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak(3)
+puppy.roll_over
+puts puppy.dog_years(10)
+puppy.play_dead(5)
 
