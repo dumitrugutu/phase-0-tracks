bcbfbf3080aaf172787d1404707d93f127024829
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-27 09:25:29 -0500

    Assign gender and ethnicity to each santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -24,3 +24,11 @@ santa.eat_milk_and_cookies("chocolate chip")
 
 reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
 	"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", 
+	"Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
