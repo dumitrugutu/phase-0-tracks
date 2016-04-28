188ef67bf859cc8e6b3c460c9789339c50f9fb69
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-28 14:19:17 -0500

    Create the espresso method

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,6 +1,6 @@
 # Designing a Coffee Machine
 # It will take 3 attributes: size, level of sweetnes, flavors 
-# and 4 methods: initialize, ice coffee, hot coffee, expresso, 
+# and 4 methods: initialize, ice coffee, hot coffee, espresso, 
 # It will print: "Initializing a new cup of coffee" 
 # for each new cup o coffee
 # It will also print the type of coffee
@@ -21,11 +21,15 @@ class CoffeeMachine
 	end
 
 	def iced_coffee
-	    puts "Working on a #{size} iced_coffee..." 
+	    puts "Working on a #{size} iced coffee..." 
 	end
 
 	def hot_coffee
 		puts "Brewing a #{size} size hot coffee..."
+	end
+
+	def espresso
+		puts "Your espresso will be ready shortly"
 	end
 
 end
