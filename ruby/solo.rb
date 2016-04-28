6b0b0a6573f9ecb7599ee78b9d3294b37e1d2f07
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-28 13:06:49 -0500

    Change name to coffee machine

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,4 +1,4 @@
-# Designing a Coffee Maker
+# Designing a Coffee Machine
 # It will take 3 attributes: size, level of sweetnes, flavors 
 # and 4 methods: initialize, ice coffee, hot coffee, expresso, 
 # It will print: "Initializing a new cup of coffee" 
