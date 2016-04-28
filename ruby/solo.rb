87a7eeccc6816679d16f4c10affd08256a83c856
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-28 13:06:02 -0500

    Design the coffee maker

---
+++ ruby/solo.rb
@@ -0,0 +1,7 @@
+# Designing a Coffee Maker
+# It will take 3 attributes: size, level of sweetnes, flavors 
+# and 4 methods: initialize, ice coffee, hot coffee, expresso, 
+# It will print: "Initializing a new cup of coffee" 
+# for each new cup o coffee
+# It will also print the type of coffee
+# Work on some lame a$$ grinding noize
