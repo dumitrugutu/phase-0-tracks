e73a65839d45dd733967294f7b459c0d8a27b1bc
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-04-29 11:39:54 -0500

    Create a module and two classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,31 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+# 	def self.yelling_happily(cheer)
+# 		cheer + "Yay!" + " :)"
+#     end
+# end
+
+# Shout.yell_angrily("Ughhh")
+# Shout.yelling_happily("My code works.")
+
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def cries_uncontrollably(nationality)
+		puts "It's the saddest day of my life, shouts the boy as he cries in #{nationality}"
 	end
-	def self.yelling_happily(cheer)
-		cheer + "Yay!" + " :)"
-    end
 end
 
-Shout.yell_angrily("Ughhh")
-Shout.yelling_happily("My code works.")
+class Spanish
+	include Shout
+end
 
+class German
+	include Shout
+end
+
+german = German.new
+german.cries_uncontrollably("german")
+
+spanish = Spanish.new
+spanish.cries_uncontrollably("his mother's arms")
