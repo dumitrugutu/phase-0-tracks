b69ce5c44a73fb2184956c778df5f7dd247abdf7
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-05-01 20:38:51 -0500

    Create methods to add, delete, update an item from the list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,54 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+list = "Milk eggs bread fruit cereal coffee"
+  # set default quantity
+  # grocery_list = {"milk" => 1, "eggs" => 1, "bread" => 1, "cereal" => 1, "coffee" => 1}
+def create_grocery_list(list)
+	grocery_list_hash = {}
+  # print the list to the console [can you use one of your other methods here?]
+  splitted_list = list.split(' ')
+  # output: [what data type goes here, array or hash?]
+  splitted_list.each do |name|
+  	grocery_list_hash[name] = 1
+  end
+  grocery_list_hash
+end
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: 
+def add_item_to_list(item, quantity)
+	@list[item] = quantity
+end
+add_item_to_list("apples", 4)
+# output:
+
+# Method to remove an item from the list
+def remove_item(item)
+	@list.delete(item)
+end
+remove_item("eggs")
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+def update_list(item, quantity)
+	@list[item] = quantity
+end
+update_list("apples", 3)
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+def print_list
+	@list.each do |name, value|
+		puts "Today you have to buy #{name} of #{value}"
+	end
+end
+print_list
+# input:
+# steps:
+# output:
