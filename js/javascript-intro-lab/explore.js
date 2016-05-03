f49da287ef497ffbb7b90e5791ab5ba7b5a14c0f
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-05-03 15:01:23 -0500

    Add the pseudocode

---
+++ js/javascript-intro-lab/explore.js
@@ -0,0 +1,8 @@
+// Reverse words
+
+// Create a function that takes a string as an argument 
+// Split the string into single letters using .split('')
+// Reverse the order of each letter using .reverse()
+// Add the letters back together using .join('')
+// Print the result
+// Call the function
