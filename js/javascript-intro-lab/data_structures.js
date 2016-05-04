7b1c0475c8ee6a2ddfd2d4b0de8ccef7c9f5f210
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-05-04 15:53:21 -0500

    Create the names and colors variables

---
+++ js/javascript-intro-lab/data_structures.js
@@ -0,0 +1,12 @@
+// create two variables
+var colors = ["red", "blue", "green", "brown"];
+var names = ["Ed", "Champion", "Bud", "Tomm"];
+
+// add another color to the array
+colors.push("white");
+// print the array
+console.log(colors);
+// add another name to the array
+names.push("Lilly");
+// print the array
+console.log(names);
