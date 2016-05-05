b193884e376bdae5895c09813d07556f7565737d
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-05-05 09:33:10 -0500

    Add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+// BUSINESS LOGIC
+
+// create an array that takes words and phrases as strings
+// create a function that loops through the array
+// and compares each string
+// return the longest word or phrase
+
+
+
+// DRIVER CODE
+
+// ["long phrase","longest phrase","longer phrase"] => "longest phrase"
