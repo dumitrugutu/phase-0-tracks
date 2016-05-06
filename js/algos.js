99ee9bf34e492d261f9a6e93d81607a48f1293c0
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-05-06 17:23:35 -0500

    Create the function that finds the longest phrase

--- js/algos.js
+++ js/algos.js
@@ -5,7 +5,20 @@
 // and compares each string
 // return the longest word or phrase
 
+var array = ["long phrase","longest phrase","longer phrase", "this is a longer phrase"];
 
+function longestPhrase(phrase) {
+    var shortest = 0;
+    var longest;
+    for (var word = 0; word < phrase.length; word++)
+    if (array[word].length > shortest) {
+    	shortest = array[word].length;
+    	longest = array[word];
+    }
+    console.log(longest);
+    	
+}
+longestPhrase(array);
 
 // DRIVER CODE
 
