388bacde20b48e63ae4de4e2cef25dce58c83f7f
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-05-08 12:05:47 -0500

    Create function that compares two objects

--- js/algos.js
+++ js/algos.js
@@ -5,8 +5,6 @@
 // and compares each string
 // return the longest word or phrase
 
-var array = ["long phrase","longest phrase","longer phrase", "this is a longer phrase"];
-
 function longestPhrase(phrase) {
     var shortest = 0;
     var longest;
@@ -15,11 +13,43 @@ function longestPhrase(phrase) {
     	shortest = array[word].length;
     	longest = array[word];
     }
-    console.log(longest);
-    	
+    console.log(longest);    	
 }
-longestPhrase(array);
+
+// create a functions that compares two objects for matching key-value pair
+// create a variable and set to false
+// iterate through the first object
+// iterate through the second object
+// if at least one matches, return true, else false
+
+function commonDetails(hash1, hash2) {
+	var matchingDetail = false;
+    var key1;
+    var key2;
+	for ( key1 in hash1 ) {
+		// console.log(key1);
+		for ( key2 in hash2) {
+			// console.log(key2);
+			if ( key1 == key2 ) {
+				if ( hash1[key1] == hash2[key2] )
+				    matchingDetail = true;
+			}
+		}
+	}
+    return matchingDetail;
+}
+
 
 // DRIVER CODE
 
-// ["long phrase","longest phrase","longer phrase"] => "longest phrase"
+// testcode for longestPhrase
+var array = ["long phrase","longest phrase","longer phrase", "this is a longer phrase"];
+longestPhrase(array);
+
+// testcode for commonDetails
+// var firstHash = { name: "Tamir", age: 55 };
+// var secondHash = { name: "Steven", age: 55 };
+var firstHash = { name: "Tamir", age: 54 };
+var secondHash = { name: "Steven", age: 55 };
+console.log( commonDetails(firstHash, secondHash) );
+
