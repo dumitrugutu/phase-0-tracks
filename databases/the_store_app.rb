5e35fd13f867fc8b920bb22a5b04cabe18e2ba65
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-05-13 18:34:28 -0500

    Add pseudocode

---
+++ databases/the_store_app.rb
@@ -0,0 +1,19 @@
+# Create a store app
+
+# require gems
+
+# Create SQLite3 database
+
+# use string delimiters to create tables
+
+# create customers and managers tables
+
+# add testcode
+
+# use ORM to retrieve data 
+
+# create method that adds customers to database
+
+# create method that authorizes managers to modify data in the customers table
+
+# add user interface
