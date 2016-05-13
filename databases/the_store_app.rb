a1598bfde0ab3cb437a1bcf4650c459f1ad150e1
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-05-13 18:37:17 -0500

    Use string delimiters to create tables

--- databases/the_store_app.rb
+++ databases/the_store_app.rb
@@ -8,6 +8,31 @@ require 'faker'
 store_database = SQLite3::Database.new("store.db")
 
 # use string delimiters to create tables
+create_table_customers = <<-SQL
+  CREATE TABLE IF NOT EXISTS customers(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    card_number INTEGER,
+    amount INTEGER
+  )
+SQL
+
+create_table_items = <<-SQL
+  CREATE TABLE IF NOT EXISTS items(
+    id INTEGER PRIMARY KEY,
+    item_name VARCHAR(255),
+    quantity INTEGER,
+    price INTEGER
+  )
+SQL
+
+create_table_managers = <<-SQL
+  CREATE TABLE IF NOT EXISTS managers(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    idn INTEGER
+  )
+SQL
 
 # create customers and managers tables
 
