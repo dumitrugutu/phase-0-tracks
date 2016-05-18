28b03d8fc30f0e7755c39d0857da51ddfd4cec69
Dumitru Gutu <dumitrugutu88@gmail.com>
2016-05-18 18:32:46 -0500

    Create the function that hides all the paragraphs

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,1 +1,8 @@
 console.log("The script is running.")
+
+//hide the paragraphs when the button is clicked
+$(document).ready(function() {
+	$("#btn1").click(function() {
+		$("p").hide("slow");
+	});
+});
